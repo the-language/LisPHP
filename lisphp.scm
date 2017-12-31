a835ffb4885d87fb153bcbc03a7d32640f97097c
zaoqi <zaomir@outlook.com>
2017-12-31 19:58:53 +0800

    .

--- lisphp.scm
+++ lisphp.scm
@@ -13,19 +13,23 @@
 
 ;;  You should have received a copy of the GNU Affero General Public License
 ;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+(define (string-append* xs) (apply string-append xs))
+
 (define (*displayln x) (string-append "echo "x))
 
 (define (*boolean? x) (string-append "is_bool("x")"))
 (define *true "true")
 (define *false "false")
 (define (*numeric? x) (string-append "is_numeric("x")"))
+(define (*int? x) (string-append "is_int("x")"))
+(define (*float? x) (string-append "is_float("x")"))
 (define (**number x)
   (if (integer? x)
       (number->string (inexact->exact x))
       (number->string (exact->inexact x))))
 (define (*string? x) (string-append "is_string("x")"))
 (define (**string x)
-  (string-append "\""(apply string-append (map **string%char (string->list x)))"\""))
+  (string-append "\""(string-append* (map **string%char (string->list x)))"\""))
 (define (**string%char c)
   (cond
     [(eq? c #\\) "\\\\"]
@@ -34,4 +38,12 @@
     [(eq? c #\$) "\\$"]
     [else (string c)]))
 
+(define (%*app xs)
+  (string-append* (add-between xs ",")))
+(define (*apply f xs) (string-append f"("(%*app xs)")"))
+
+
+(define (*vector? x) (string-append "is_array("x")"))
+(define (*object? x) (string-append "is_object("x")"))
+(define (*vector* xs) (string-append "array("(%*app xs)")"))
   
