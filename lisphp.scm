a66cb3f1f9a06d71334c610644bcbe97c0329f10
zaoqi <zaomir@outlook.com>
2017-12-31 19:48:56 +0800

    .

--- lisphp.scm
+++ lisphp.scm
@@ -13,3 +13,25 @@
 
 ;;  You should have received a copy of the GNU Affero General Public License
 ;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+(define (*displayln x) (string-append "echo "x))
+
+(define (*boolean? x) (string-append "is_bool("x")"))
+(define *true "true")
+(define *false "false")
+(define (*numeric? x) (string-append "is_numeric("x")"))
+(define (**number x)
+  (if (integer? x)
+      (number->string (inexact->exact x))
+      (number->string (exact->inexact x))))
+(define (*string? x) (string-append "is_string("x")"))
+(define (**string x)
+  (string-append "\""(apply string-append (map **string%char (string->list x)))"\""))
+(define (**string%char c)
+  (cond
+    [(eq? c #\\) "\\\\"]
+    [(eq? c #\newline) "\\n"]
+    [(eq? c #\") "\\\""]
+    [(eq? c #\$) "\\$"]
+    [else (string c)]))
+
+  
